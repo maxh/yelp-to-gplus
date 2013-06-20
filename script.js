85ab1b93ec5ceda2515fc68c1357cda31bfb4495
Max Heinritz <maxheinritz@gmail.com>
2013-06-19 22:31:36 -0700

    paginated requests

--- script.js
+++ script.js
@@ -1,38 +1,66 @@
-var request = require('request');
-var $ = require('cheerio');
+var request, $, fs;  // Packages.
+request = require('request');
+$ = require('cheerio');
+fs = require('fs');
 
-var options = {
-    uri: 'http://maxheinritz.yelp.com',
-};
+var domain, username, reviews;  // Global variables.
+domain = 'yelp.com';
+username = 'maxheinritz';
+reviews = [];
 
 callback = function (error, response, body) {
     if (error || response.statusCode != 200) {
 	return console.log(error);
     }
-    var parsedHTML, reviews;
+    var parsedHTML, nextPath; // Hoist variables.
     parsedHTML = $.load(body);
-    reviews = [];
+    // The review text and metadata are within divs with this class.
     parsedHTML('.review.clearfix').map(function(i, rawDiv) {
-	var review = {};
-	review.businessAddress = clean($(rawDiv).find('.biz_info address').html());
-	review.businessName = clean($(rawDiv).find('.biz_info h4 a').html());;
-	review.rating = clean($(rawDiv).find('.review-meta .rating-container .rating i').attr('title')[0]);
-	review.reviewText = clean($(rawDiv).find('.review_comment').html());
-	reviews.push(review);
+	// For each review, create an associated Review JS object.
+	reviews.push(new Review(rawDiv));
     });
-
-    // TODO: Retrieve the next page in the paginated list of reviews.
-
+    
+    // Find the link to the next page of reviews./
+    nextPath = parsedHTML(
+	'#user_reviews_list p .ybtn.ybtn-secondary.ybtn-small')
+	.first().attr('href');
+    if (!nextPath)
+	nextPath = parsedHTML('#pager_page_next').first().attr('href');
+    if (!nextPath) // No next page, stop crawling.
+	return;
+    // Request the next page of reviews.
+    sendReviewPageRequest('http://' + domain + nextPath);
     console.log(reviews);
 }
 
-function clean (inputString) {
-    return inputString.replace(/<br>/g,' ')
-	.replace(/(\n|\t|<.*?>| $)/g,'');
+sendReviewPageRequest = function (URL) {
+    var stdin = process.stdin, stdout = process.stdout;
+    
+    stdin.resume();
+    stdout.write('Now sending request to: ' + URL + "\n");
+    stdout.write('OK? (y/n) > ');
+
+    stdin.once('data', function(data) {
+	data = data.toString().trim();
+	if (data == 'y') {
+	    request(URL, callback);
+	}
+    });
 }
 
-console.log('sending request');
+// JavaScript object representing a review and its metadata.
+function Review (rawDiv) {
+    this.businessAddress = clean($(rawDiv).find('.biz_info address').html());
+    this.businessName = clean($(rawDiv).find('.biz_info h4 a').html());;
+    this.rating = clean($(rawDiv).find(
+	'.review-meta .rating-container .rating i').attr('title')[0]);
+    this.reviewText = clean($(rawDiv).find('.review_comment').html());
+}
 
-request(options, callback);
+function clean (inputString) {
+    return inputString.replace(/<br>/g,' ').replace(/(\n|\t|<.*?>| $)/g,'');
+}
+
+sendReviewPageRequest('http://' + username + '.' + domain);
 
 // TODO: Search G+ Local for restaurant ID, programmatically write review.
