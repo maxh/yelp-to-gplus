5efdde91281169609c0d4e6f21a6be7f158b4b36
Max Heinritz <maxheinritz@gmail.com>
2013-07-05 16:17:57 -0700

    finish google plus integration flow

--- script.js
+++ script.js
@@ -1,66 +1,167 @@
-var request, $, fs;  // Packages.
-request = require('request');
-$ = require('cheerio');
-fs = require('fs');
+var $ = require('cheerio');
+var cp = require('child_process');
+var fs = require('fs');
+var open = require('open');
+var request = require('request');
 
-var domain, username, reviews;  // Global variables.
-domain = 'yelp.com';
-username = 'maxheinritz';
-reviews = [];
+var stdin = process.stdin, stdout = process.stdout;
 
-callback = function (error, response, body) {
-    if (error || response.statusCode != 200) {
-	return console.log(error);
-    }
-    var parsedHTML, nextPath; // Hoist variables.
-    parsedHTML = $.load(body);
-    // The review text and metadata are within divs with this class.
-    parsedHTML('.review.clearfix').map(function(i, rawDiv) {
-	// For each review, create an associated Review JS object.
-	reviews.push(new Review(rawDiv));
-    });
-    
-    // Find the link to the next page of reviews./
-    nextPath = parsedHTML(
-	'#user_reviews_list p .ybtn.ybtn-secondary.ybtn-small')
-	.first().attr('href');
-    if (!nextPath)
-	nextPath = parsedHTML('#pager_page_next').first().attr('href');
-    if (!nextPath) // No next page, stop crawling.
-	return;
-    // Request the next page of reviews.
-    sendReviewPageRequest('http://' + domain + nextPath);
-    console.log(reviews);
-}
 
-sendReviewPageRequest = function (URL) {
-    var stdin = process.stdin, stdout = process.stdout;
-    
-    stdin.resume();
-    stdout.write('Now sending request to: ' + URL + "\n");
-    stdout.write('OK? (y/n) > ');
+// Crawls {USERNAME}'s Yelp reviews, saves them in JSON in $PWD/{OUTFILE}.
+function scrapeReviewsFromYelp() {
+  var DOMAIN = 'yelp.com';
+  var USERNAME = 'maxheinritz';
+  var OUTFILE = 'reviews.out';
 
-    stdin.once('data', function(data) {
-	data = data.toString().trim();
-	if (data == 'y') {
-	    request(URL, callback);
-	}
-    });
-}
+  var reviews = [];
 
-// JavaScript object representing a review and its metadata.
-function Review (rawDiv) {
+  // JavaScript class representing a review and its metadata.
+  function ScrapedReview(rawDiv) {
     this.businessAddress = clean($(rawDiv).find('.biz_info address').html());
     this.businessName = clean($(rawDiv).find('.biz_info h4 a').html());;
     this.rating = clean($(rawDiv).find(
-	'.review-meta .rating-container .rating i').attr('title')[0]);
+                        '.review-meta .rating-container .rating i')
+                        .attr('title')[0]);
     this.reviewText = clean($(rawDiv).find('.review_comment').html());
+  }
+
+  // Helper function to clean crawled HTML.
+  function clean(inputString) {
+    return inputString.replace(/<br>/g,' \n ').replace(/(\n|\t|<.*?>)/g,'').trim();
+  }
+
+  // Callback called when response from Yelp is received.
+  callback = function (error, response, body) {
+    if (error || response.statusCode != 200)
+      return console.log(error);
+    var parsedHTML = $.load(body);
+    
+    // The review text and metadata are within divs with this class.
+    parsedHTML('.review.clearfix').map(function (i, rawDiv) {
+      // For each review, create an associated ScrapedReview JS object.
+      reviews.push(new ScrapedReview(rawDiv));
+    });
+    console.log('== We now have ' + reviews.length + ' reviews!');
+
+    // Find the link to the next page of reviews.
+    var nextPath = parsedHTML('#user_reviews_list p .ybtn.ybtn-secondary.ybtn-small')
+      .first().attr('href');
+    if (!nextPath)
+      nextPath = parsedHTML('#pager_page_next').first().attr('href');
+
+    if (nextPath) {
+      // Request the next page of reviews.
+      promptUserToSendReviewPageRequest('http://' + DOMAIN + nextPath);
+    }
+    else { // No next page, stop crawling.
+      console.log('== All reviews scraped!');
+      fs.writeFileSync(OUTFILE, JSON.stringify(reviews), {encoding: 'utf8'});
+      promptForGoogle(); // Weird place to move to next part of execution...
+    }
+  }
+
+  // Sends a request to Yelp, with the user's permission.
+  promptUserToSendReviewPageRequest = function (URL) {
+    stdin.resume();
+    stdout.write('== Now sending request to: ' + URL + '\n');
+    stdout.write('== OK? (y/n/q) > ');
+
+    stdin.once('data', function(data) {
+      stdin.pause();
+      data = data.toString().trim();
+      if (data == 'y') {
+        request(URL, callback);
+      }
+      else if (data == 'q') {
+        promptForGoogle(); // Weird place to move to next part of execution...
+      }
+      else {
+        console.log('== Data so far:');
+        console.log(reviews);
+        sendReviewPageRequest(URL);
+      }
+    });
+  }
+
+  // Send the first request.
+  promptUserToSendReviewPageRequest('http://' + USERNAME + '.' + DOMAIN);
 }
 
-function clean (inputString) {
-    return inputString.replace(/<br>/g,' ').replace(/(\n|\t|<.*?>| $)/g,'');
+// Opens the G+ Local page for each review and prompts the user to manually 
+// enter their review through the web interface.
+function addReviewstoGooglePlus(startIndex) {
+  var INFILE = 'reviews.out';
+  var URL = 'http://plus.google.com/local/';
+  var reviews;
+
+  reviews = JSON.parse(fs.readFileSync(INFILE, 'utf-8'));
+
+  function loadGooglePlusPageForAction(i) {
+    if (i == reviews.length)
+      return;
+
+    cp.exec('pbcopy << EOF\n' + reviews[i].reviewText + '\nEOF',
+      function(err, sout, serr) {
+        stdin.resume();
+        console.log('== Please paste the review for ' + reviews[i].businessName + 
+                    ' in your browser.');
+        console.log('Rating: ' + reviews[i].rating);
+        console.log('Review (copied to clipboard): ' + reviews[i].reviewText);
+        console.log('Only ' + (reviews.length - i) + ' reviews left!');
+        stdout.write('== Press enter when done. > ');
+        open(URL + reviews[i].businessAddress + '/s/' + reviews[i].businessName);
+        stdin.once('data', function (data) {
+          stdin.pause();
+          loadGooglePlusPageForAction(i+1);
+          // No unoptimized tail recursion problem here because the original call to 
+          // loadGooglePlusPageForAction(i) was removed from memory when its synchr-
+          // onous part finished. (At least, I think that's the case.)
+        });
+      }
+    );
+  }
+
+  loadGooglePlusPageForAction(startIndex);
 }
 
-sendReviewPageRequest('http://' + username + '.' + domain);
+function promptForYelp() {
+  stdin.resume();
+  stdout.write('== Would you like to scrape Yelp? (y/n) > ');
+  stdin.once('data', function(data) {
+    stdin.pause();
+    data = data.toString().trim();
+    if (data == 'y') {
+      scrapeReviewsFromYelp(); // Async, so instead of returning, it calls
+                               // promptForGoogle() inside its code.
+    }
+    else {
+      promptForGoogle();
+    }
+  });
+}
 
-// TODO: Search G+ Local for restaurant ID, programmatically write review.
+function promptForGoogle() {
+  stdin.resume();
+  stdout.write('== Would you like to add reviews G+? (y/n) > ');
+  stdin.once('data', function(data) {
+    stdin.pause();
+    data = data.toString().trim();
+    if (data == 'y') {
+      stdin.resume();
+      stdout.write('== Would you like to start at a specific review ' + 
+                   'number (default=0)? > ');
+      stdin.once('data', function(data) {
+        stdin.pause();
+        data = data.toString().trim();
+        if (parseInt(data) != NaN) {
+          addReviewstoGooglePlus(parseInt(data));
+        }
+        else {
+          addReviewstoGooglePlus(0);    
+        }
+      });
+    }
+  });
+}
+
+promptForYelp();
